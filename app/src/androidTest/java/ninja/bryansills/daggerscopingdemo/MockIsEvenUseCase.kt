a677b40a2c0d780f82f7af54288e551cef2d3e60
bryansills <bsills@bignerdranch.com>
2024-02-21 15:46:45 -0600

    Bad tests!

--- app/src/androidTest/java/ninja/bryansills/daggerscopingdemo/MockIsEvenUseCase.kt
+++ app/src/androidTest/java/ninja/bryansills/daggerscopingdemo/MockIsEvenUseCase.kt
@@ -1,10 +1,11 @@
 package ninja.bryansills.daggerscopingdemo
 
 import app.cash.turbine.test
+import assertk.assertThat
+import assertk.assertions.isTrue
 import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.mockk
-import junit.framework.TestCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
@@ -25,7 +26,20 @@ class MockIsEvenUseCase {
         create(10)
 
         isEvenUseCase().test {
-            TestCase.assertTrue(awaitItem())
+            assertThat(awaitItem()).isTrue()
+            counterUseCase.increment()
+//            TestCase.assertFalse(awaitItem()) MOCKING IS DUMB THIS WON'T WORK
+            ensureAllEventsConsumed()
+            cancel()
+        }
+    }
+
+    @Test
+    fun ughTest() = runTest {
+        create(11)
+
+        isEvenUseCase().test {
+            assertThat(awaitItem()).isTrue()
             counterUseCase.increment()
 //            TestCase.assertFalse(awaitItem()) MOCKING IS DUMB THIS WON'T WORK
             ensureAllEventsConsumed()
