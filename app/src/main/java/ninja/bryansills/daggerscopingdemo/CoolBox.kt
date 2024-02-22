9e9fa31af0fa30c6d6345649a6b7cf232ea4dab7
bryansills <bsills@bignerdranch.com>
2024-02-22 16:53:51 -0600

    Add Paparazzi to the project

---
+++ app/src/main/java/ninja/bryansills/daggerscopingdemo/CoolBox.kt
@@ -0,0 +1,45 @@
+package ninja.bryansills.daggerscopingdemo
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CoolBox() {
+    Box(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .size(200.dp)
+    ) {
+        Text(
+            text = "This text is on the left",
+            modifier = Modifier.align(Alignment.CenterStart)
+        )
+
+        Box(
+            modifier = Modifier
+                .background(Color.Green)
+                .size(50.dp)
+                .align(Alignment.BottomCenter)
+        )
+
+        Text(
+            text = "This text is on the right",
+            color = Color.Magenta,
+            modifier = Modifier.align(Alignment.TopEnd)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CoolBoxPreview() {
+    CoolBox()
+}
--- app/src/main/java/ninja/bryansills/daggerscopingdemo/MainActivity.kt
+++ app/src/main/java/ninja/bryansills/daggerscopingdemo/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -17,10 +16,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
