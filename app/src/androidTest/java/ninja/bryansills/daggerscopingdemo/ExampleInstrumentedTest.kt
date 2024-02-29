56789077d25acb19ca7f3c7800269ba6851f3d2b
bryansills <bsills@bignerdranch.com>
2024-02-29 17:44:11 -0600

    Tests are good again

--- app/src/androidTest/java/ninja/bryansills/daggerscopingdemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ninja/bryansills/daggerscopingdemo/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ninja.bryansills.fail", appContext.packageName)
+        assertEquals("ninja.bryansills.daggerscopingdemo", appContext.packageName)
     }
 }
